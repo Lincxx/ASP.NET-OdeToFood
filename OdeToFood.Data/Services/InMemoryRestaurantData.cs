3dc6f55c81f0d5df87a33871d0415718680ec747
Jeremy Lincoln <jeremy.lincoln@bisonline.com>
2022-05-31 16:59:28 -0400

    Added a Data project along with some new classes

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,26 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Jeremy's Pizza", Cuisine = CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Shahibs", Cuisine = CuisineType.Indian},
+                new Restaurant { Id = 3, Name = "FrieBurgs", Cuisine = CuisineType.None}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
